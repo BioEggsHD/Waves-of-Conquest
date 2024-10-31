fb59e589faf191b49d5dfbcbc1ba07d7883ad612
BioEggsHD <alisup221@gmail.com>
2024-10-31 10:12:24 +0100

    Create a-bwoc-init.cpp

---
+++ a-bwoc-init.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <array>
+using namespace std;
+int main() {
+  //
+  cout<<"Initialized"<<endl;
+  cout<<endl;
+  return 0;
+}
+int upd(double: delta) {
+  //
+  return 0;
+}
+int _updf(double: delta) {
+  //
+  return 0;
+}
+new void = mainstr(args: {(double: delta),(array: ),(str: un)}) {
+  //
+  return NULL;
+};
